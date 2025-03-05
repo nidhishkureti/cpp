581e0745e35b9d457e4ffe0221bf7a968bee8689
nidhishkureti <nidhishkureti@gmail.com>
2025-03-05 11:41:24 +0530

    commited

---
+++ 956A.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb emplace_back
+#define vi vector<int>
+#define ll long long
+#define vll vector<ll>
+#define ull unsigned long long
+#define ui unsigned int
+#define vp(a, b) vector<pair<(a), (b)>>
+#define mems(a, x) memset((a), (x), sizeof(a))
+#define fo(n, i, start) for (ll i = start; i <= n; i++)
+#define f(n) for (ll i = 0; i < n; i++)
+#define all(a) a.begin(), a.end()
+#define allr(a) a.rbegin(), a.rend()
+const ll MOD = 1e9 + 7;
+void solve()
+{
+int n;
+cin>>n;
+
+f(n) cout<<i+1<<" ";
+cout<<endl;
+
+
+
+}
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+cout.tie(NULL);
+ll tt ;
+cin >> tt;
+while (tt--)
+{
+solve();
+}
+return 0;
+}
