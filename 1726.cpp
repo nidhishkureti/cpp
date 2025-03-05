1197800a64d0fbd70ed79fd8175e251557be0eb0
nidhishkureti <nidhishkureti@gmail.com>
2025-03-05 11:30:00 +0530

    Updated 1726.cpp with new changes

---
+++ 1726.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb emplace_back
+#define vi vector<int>
+#define ll long long
+#define vll vector<ll>
+#define ull unsigned long long
+#define ui unsigned int
+#define vp(a, b) vector<pair<(a), (b)>>
+#define mems(a, x) memset((a), (x), sizeof(a))
+#define fo(n, i, start) for (ll i = start; i <= n; i++)
+#define f(n) for (ll i = 0; i < n; i++)
+#define all(a) a.begin(), a.end()
+#define allr(a) a.rbegin(), a.rend()
+const ll MOD = 1e9 + 7;
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+void solve()
+{
+}
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+cout.tie(NULL);
+ll tt ;
+cin >> tt;
+while (tt--)
+{
+solve();
+}
+return 0;
+}
